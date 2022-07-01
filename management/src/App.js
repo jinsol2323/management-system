f35d6874c46af9e05edb1562850f80030c47b091
sung@nineonesoft.com <sung@nineonesoft.com>
2022-07-01 10:01:09 +0900

    Create Customer.js

--- management/src/App.js
+++ management/src/App.js
@@ -1,13 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let's develop management system!</h2>
-    </div>
-  );
+
+
+const customers = [
+  {
+    'id':1,
+    'image':'http://placeimg.com/640/480/any"',
+    'name':'현빈',
+    'birthday':'961222',
+    'gender':'남자',
+    'job':'대학생',
+
+},
+{
+  'id':2,
+  'image':'http://placeimg.com/640/480/any"',
+  'name':'홍길동',
+  'birthday':'961222',
+  'gender':'남자',
+  'job':'대학생',
+
+},
+{
+  'id':3,
+  'image':'http://placeimg.com/640/480/any"',
+  'name':'김길동',
+  'birthday':'961222',
+  'gender':'남자',
+  'job':'대학생',
+
+},
+]
+
+
+class App extends Component{
+    render() {
+      return(
+        <div>
+          {
+            customers.map(c=>{
+              return(
+                <Customer 
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              )
+            })            
+          }
+        </div>
+      )
+    }
 }
 
 export default App;
