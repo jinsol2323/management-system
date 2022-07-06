c2262ba67568f7aa66a8ad641b257417242b913c
sung@nineonesoft.com <sung@nineonesoft.com>
2022-07-06 14:01:39 +0900

    Update UI/UX Add Customer Search Module

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,16 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import CustomerAdd from './components/CustomerAdd';
 
 
+import {  alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+
 /*
 react LifeCycle
 
@@ -28,12 +38,18 @@ react LifeCycle
 
 */
 
+
+
 const styles = theme => ({
   root:{
-    width : '100%',
+    width : 'auto',
     marginTop : theme.spacing(3),
     overflowX: 'auto',
-    height:'600px'
+  },
+
+  paper:{
+    marginLeft:18,
+    marginRight:18
   },
 
   table:{
@@ -42,28 +58,83 @@ const styles = theme => ({
   
   progress:{
     margin:theme.spacing(2),
-  }
+  },
+
+  menu:{
+    marginTop:15,
+    marginBottom:20,
+    display:'flex',
+    justifyContent:'center',
+  },
+
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px) !important`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
 }); 
 
-class App extends Component{
 
-    // state = {
-    //   customers:"",
-    //   completed: 0
-    // }
+class App extends Component{
 
     constructor(props){
       super(props);
       this.state={
         customers:"",
-        completed: 0
+        completed: 0,
+        searchKeyword:''
       }
     }
 
     stateRefresh = () =>{
       this.setState({
         customees:'',
-        completed:0
+        completed:0,
+        searchKeyword:''
       });
       this.callApi() 
       .then(res => this.setState({customers:res}))
@@ -91,11 +162,67 @@ class App extends Component{
       this.setState({completed:completed >= 100 ? 0 : completed+1});
     }
 
+    handleValueChange = (e) =>{
+      let nextState={};
+      nextState[e.target.name] = e.target.value;
+      this.setState(nextState); 
+    }
+
     render() {
+      const filteredComponents = (data) =>{
+        data = data.filter((c)=>{
+          return c.name.indexOf(this.state.searchKeyword) > -1;
+        })
+        return data.map((c)=>{
+          return <Customer stateRefresh={this.stateRefresh}
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+        })
+      }
       const {classes} = this.props?this.props:null;
       return(
-        <div>
-          <Paper className={classes.root}>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="open drawer"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography className={classes.title} variant="h6" noWrap>
+                고객관리 Management
+              </Typography>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  placeholder="검색하기"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                  name="searchKeyword"
+                  value={this.state.seachKeyword}
+                  onChange={this.handleValueChange}
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </div>
+            </Toolbar>
+          </AppBar>
+          <div className={classes.menu}>
+            <CustomerAdd stateRefresh={this.stateRefresh}/>
+          </div>
+          <Paper className={classes.paper}>
             <Table className={classes.table}>
               <TableHead>
                 <TableRow>
@@ -109,18 +236,8 @@ class App extends Component{
                 </TableRow>
               </TableHead>
               <TableBody>
-                {this.state.customers ? this.state.customers.map(c=>{
-                return(<Customer stateRefresh={this.stateRefresh}
-                    key={c.id}
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday={c.birthday}
-                    gender={c.gender}
-                    job={c.job}
-                  />
-                )}
-              ):
+                {this.state.customers ? 
+                filteredComponents(this.state.customers):
               <TableRow>
                 <TableCell colSpan="6" align='center'>
                   <CircularProgress className={classes.progress} value={this.state.completed}></CircularProgress>
@@ -131,7 +248,6 @@ class App extends Component{
               </TableBody>
             </Table>       
           </Paper>
-          <CustomerAdd stateRefresh={this.stateRefresh}/>
         </div>
       )
     }
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider,createTheme } from '@material-ui/core/styles';
+
+
+const theme = createTheme({
+  typography:{
+    fontFamily:"'Noto Sans KR',sans-serif"
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <createTheme theme={theme}>
   <React.StrictMode>
-    <App />
+      <App /> 
   </React.StrictMode>
+  </createTheme>
 );
 
 // If you want to start measuring performance in your app, pass a function
